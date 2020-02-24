8a940ef3b82f2eaefc634c5773c89fd9e0b4a29e
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-02-24 09:35:22 +0100

    Add project files.

---
+++ S2.Oop/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.Oop
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ S2.Oop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S2.Oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
